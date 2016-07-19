7016fe28901e213416296bafbeee97422103654b
YOUNG <young@youngs-macbook-pro.local>
2016-07-20 01:33:46 +0800

    c

---
+++ Budejie/Budejie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Budejie
+//
+//  Created by YOUNG on 16/7/20.
+//  Copyright © 2016年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
