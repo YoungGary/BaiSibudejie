75df2d3f20dcc99bbf92db366cc7117f3f5b9d22
YOUNG <young@youngs-macbook-pro.local>
2016-07-23 00:38:28 +0800

    主体结构完成

--- Budejie/Budejie/GYNaviController.m
+++ Budejie/Budejie/GYNaviController.m
@@ -7,7 +7,11 @@
 //
 
 #import "GYNaviController.h"
+@interface GYNaviController()<UIGestureRecognizerDelegate>
 
+
+
+@end
 @implementation GYNaviController
 
 +(void)load{
@@ -18,5 +22,52 @@
     //导航条背景图片
     [bar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
 }
+
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    //全屏返回
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self.interactivePopGestureRecognizer.delegate action:@selector(handleNavigationTransition:)];
+    [self.view addGestureRecognizer:pan];
+    pan.delegate = self;
+    self.interactivePopGestureRecognizer.enabled = NO;
+    
+}
+
+-(BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer{
+    return self.childViewControllers.count>1;
+}
+
+
+-(void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
+    //统一设置返回按钮样式
+    if (self.childViewControllers.count > 0 ) {
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [button setTitle:@"返回" forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+        [button setImage:[UIImage imageNamed:@"navigationButtonReturn"] forState:UIControlStateNormal];
+        [button setImage:[UIImage imageNamed:@"navigationButtonReturnClick"] forState:UIControlStateHighlighted];
+        [button sizeToFit];
+        button.contentEdgeInsets=UIEdgeInsetsMake(0, -20, 0, 0);
+        [button addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:button];
+    }
+    
+    [super pushViewController:viewController animated:animated];
+}
+
+-(void)back{
+    [self popViewControllerAnimated:YES];
+}
+
+
+
+
+
+
+
+
+
+
 
 @end
