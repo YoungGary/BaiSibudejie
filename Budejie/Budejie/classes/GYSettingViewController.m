2c84b66febe56744aaff07bb453c9ba3e56d3a3e
YOUNG <young@youngs-macbook-pro.local>
2016-07-30 01:25:10 +0800

    c

--- Budejie/Budejie/classes/GYSettingViewController.m
+++ Budejie/Budejie/classes/GYSettingViewController.m
@@ -7,92 +7,118 @@
 //
 
 #import "GYSettingViewController.h"
-
+#import <SDWebImage/SDImageCache.h>
+static NSString * const identifier = @"cell";
 @interface GYSettingViewController ()
 
 @end
 
 @implementation GYSettingViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:identifier];
+    self.tableView.tableFooterView = [[UIView alloc ]init];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 #pragma mark - Table view data source
 
+
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+
+    return 1;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
     
-    // Configure the cell...
+   
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+   NSInteger totalSize =  [self getCacheSize];//自己算的cache
+    //sdwebimage帮我们计算缓存
+   // NSInteger size = [SDImageCache sharedImageCache].getSize;
+   // NSLog(@"sdwebIMage=%ld",size);
+    NSString *baseStr  = @"清除缓存";
     
+    if (totalSize>1000 *1000) {
+        CGFloat size = totalSize/1000/1000;
+        baseStr = [NSString stringWithFormat:@"%@(%.1fMB)",baseStr,size];
+    }else if(totalSize>1000){
+        CGFloat size = totalSize/1000;
+        baseStr = [NSString stringWithFormat:@"%@(%.1fKB)",baseStr,size];
+    }else if(totalSize >1){
+         baseStr = [NSString stringWithFormat:@"%@(%.ldB)",baseStr,totalSize];
+    }
+     cell.textLabel.text = baseStr;
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath  {
+    //点击cell删除缓存
+    NSFileManager *manager = [NSFileManager defaultManager];
+    NSString *cache = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    NSArray *file = [manager contentsOfDirectoryAtPath:cache error:nil];
+    for (NSString *filePath in file) {
+        NSString *fileTotalPath = [cache stringByAppendingPathComponent:filePath];
+        [manager removeItemAtPath:fileTotalPath error:nil];
+        
+    }
+    [self.tableView reloadData];
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+
+-(NSInteger)getCacheSize{//自己算cache有多少
+    NSFileManager *manager = [NSFileManager defaultManager];
+    NSString *cache = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *defaultPath = [cache stringByAppendingPathComponent:@"default"];
+    
+    NSArray *filePath = [manager subpathsAtPath:defaultPath];
+    //NSLog(@"%@",filePath);
+    NSInteger totalSize = 0;
+    for (NSString *path in filePath) {
+        NSString *totalPath = [defaultPath stringByAppendingPathComponent:path];
+        //过滤.DS
+        if ([totalPath containsString:@".DS"]) {
+            continue;
+        }
+        //判断是否是文件夹
+        BOOL isDirectory;
+        BOOL exist = [manager fileExistsAtPath:totalPath isDirectory:&isDirectory];
+        if (!exist || isDirectory) {
+            continue;
+        }
+        
+        
+        NSDictionary *dict = [manager attributesOfItemAtPath:totalPath error:nil];
+        totalSize += [dict fileSize];
+        
+    }
+    return totalSize;
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
+
+
+
+
+
+
+
+
+
 
 @end
